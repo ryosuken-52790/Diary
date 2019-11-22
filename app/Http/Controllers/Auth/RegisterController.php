e6f37a0310a6340c0df6f1b9ea8ef8c794d7b70b
ryosuke nagaoka <>
2019-11-22 11:18:46 +0800

    a.
    画像を入れれるようにした。

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -71,10 +71,30 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+
+        $imgPath = 
+            $this->saveProfileImage(($data['picture']));
+        // dd($imgPath);
+
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'picture_path' => $imgPath,
         ]);
+    }
+
+    // プロフィール画像を保存するためのメソッド
+    // 引数 $image : 保存したい画像をもらってくるイメージ
+    // メソッドは書いてるだけ。実行はまた別。
+    // 実行は上のcreateでやる！！
+    private function saveProfileImage($image)
+    {
+        $imgPath = $image->store('images/profilePicture', 'public');
+        // ララベルがstoreで絶対被らないように設定してくれてる。
+        // 保存した後、そのファイルまでのパスを返してくれる。
+        // ちなみにファイルはstorage/public/images/profilePicture
+
+        return 'strategy/' . $imgPath;
     }
 }
